438cf69e64cad8212cb7e14f0528ba4add831670
negin-khademi <negin.khademi@gmail.com>
2025-07-03 17:54:39 +0330

    temp : work on dark mode theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { Component, effect, signal } from "@angular/core";
+
 import { ButtonComponent } from "./shared/button/button.component";
 import { CardComponent } from "./card/card.component";
-import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import data from "../data.json";
 
@@ -13,4 +14,26 @@ import data from "../data.json";
 export class AppComponent {
 	title = "Browser_extension_managerUI";
 	data = data;
+
+	darkMode = signal<boolean>(false);
+
+	constructor() {
+		// Load theme on startup
+		const saved = localStorage.getItem("theme");
+		if (saved === "dark") {
+			this.darkMode.set(true);
+		}
+		effect(() => {
+			const isDark = this.darkMode();
+			const root = document.documentElement;
+
+			if (isDark) {
+				root.classList.add("dark");
+				localStorage.setItem("theme", "dark");
+			} else {
+				root.classList.remove("dark");
+				localStorage.setItem("theme", "light");
+			}
+		});
+	}
 }
---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: "class", // Enable manual dark mode toggle
+	content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"], // Adjust paths to match your files
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
