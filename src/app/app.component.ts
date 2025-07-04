d1ca204b2f793a3af3706d07cf87a90412c53067
negin-khademi <negin.khademi@gmail.com>
2025-07-04 15:30:14 +0330

    feat : implement toggle button slider and filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from "@angular/core";
+import { Component, OnInit, signal } from "@angular/core";
 
 import { ButtonComponent } from "./shared/button/button.component";
 import { CardComponent } from "./card/card.component";
@@ -11,9 +11,46 @@ import data from "../data.json";
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = "Browser_extension_managerUI";
 	data = data;
+	showList: any[] = [];
+	activeList: any[] = [];
+	InActiveList: any[] = [];
+
+	ngOnInit(): void {
+		this.onAllExtension();
+		// this.onctiveExtensions();
+		// this.onInActiveExtensions();
+	}
+	// show all extension
+	onAllExtension() {
+		this.showList = data;
+	}
+
+	// filter active extensions
+	onctiveExtensions() {
+		for (let obj of data) {
+			if (obj.isActive === true) {
+				this.activeList.push(obj);
+			}
+		}
+		this.showList = this.activeList;
+		this.activeList = [];
+		console.log(this.activeList);
+	}
+
+	// filter InActive extensions
+	onInActiveExtensions() {
+		for (let obj of data) {
+			if (obj.isActive === false) {
+				this.InActiveList.push(obj);
+			}
+		}
+		this.showList = this.InActiveList;
+		this.InActiveList = [];
+		console.log(this.InActiveList);
+	}
 
 	theme = signal<"light" | "dark">("light");
 
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
 	selector: "app-button",
@@ -8,4 +8,10 @@ import { Component, Input } from "@angular/core";
 })
 export class ButtonComponent {
 	@Input({ required: true }) name!: string;
+	@Output() select = new EventEmitter();
+
+	onClick() {
+		this.select.emit();
+		console.log("hello from component button");
+	}
 }
