58658bbeacd9789f60dc61b4058568b1caae38ec
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-06 21:10:11 +0330

    fix: fix ui of buttons

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -3,17 +3,17 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 @Component({
-	selector: "app-button",
-	imports: [CommonModule],
-	templateUrl: "./button.component.html",
-	styleUrl: "./button.component.scss",
+  selector: "app-button",
+  imports: [CommonModule],
+  templateUrl: "./button.component.html",
+  styleUrl: "./button.component.scss",
 })
 export class ButtonComponent {
-	@Input({ required: true }) name!: string;
-	@Input() active: boolean = false;
-	@Output() select = new EventEmitter();
+  @Input({ required: true }) name!: string;
+  @Input() active: boolean = false;
+  @Output() select = new EventEmitter();
 
-	onClick() {
-		this.select.emit(this.name);
-	}
+  onClick() {
+    this.select.emit(this.name);
+  }
 }
