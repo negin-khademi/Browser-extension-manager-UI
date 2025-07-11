1cebd6edcef7bccffbcff3d5dec4202534a31d58
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-11 12:04:00 +0330

    feat: exprot router and add middleware defenition in router

--- routes/extensionRoutes.js
+++ routes/extensionRoutes.js
@@ -3,10 +3,7 @@ const extensionController = require("../controllers/extensionController");
 
 const router = express.Router();
 
-// Read extensions data
-const extensions = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/extensions.json`, "utf-8")
-);
+router.param("id", extensionController.checkID);
 
 router.route("/").get(extensionController.getAllExtensions);
 
@@ -14,3 +11,5 @@ router
   .route("/:id")
   .delete(extensionController.deleteExtension)
   .patch(extensionController.updateExtensionStatus);
+
+module.exports = router;
